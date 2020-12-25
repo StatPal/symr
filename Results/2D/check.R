87315b170589d24c3a521d714f0705f3bf55f159
Subrata <subrata@iastate.edu>
2020-12-25 00:20:55 -0600

    Deleted files

--- Results/2D/check.R
+++
@@ -1,4 +0,0 @@
-abc <- read.csv("Performances_29_all_3_2D.csv", header=F)
-bcd <- read.csv("~/R/Researchs/MRI/Git/synthetic_mri_1/TRY_EIGEN_6_phantom/result/Performances_29_all_3.csv", header=F)
-
-summary(abc - bcd)
