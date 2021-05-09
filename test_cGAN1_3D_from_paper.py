7a8530f7223dc0afc9fb61dba13731c0a8fb2a3b
Subrata <subrata@iastate.edu>
2021-05-09 14:17:35 -0500

    Intermediate stage cGAN

--- test_cGAN1_3D_from_paper.py
+++ test_cGAN1_3D_from_paper.py
@@ -24,16 +24,12 @@ from keras.optimizers import Adam
 
 
 import tensorflow as tf
-#config = tf.ConfigProto(intra_op_parallelism_threads=15, 
-#                        inter_op_parallelism_threads=15, 
-#                        allow_soft_placement=True,
-#                        device_count = {'CPU': 15})
-#session=tf.Session(config=config) 
-
 tf.config.threading.set_intra_op_parallelism_threads(1)
 tf.config.threading.set_inter_op_parallelism_threads(15)
 
 run_opts = tf.compat.v1.RunOptions(report_tensor_allocations_upon_oom = True)
+
+
 
 
 from keras.utils.vis_utils import plot_model
@@ -64,40 +60,23 @@ from pandas import read_csv
 
 # create list of vegetation files to be opened
 # VegList = sorted(glob.glob('../brainweb_new/result/generated_r_5_*.csv'))
-VegList = sorted(glob.glob('../brainweb_new/result/generated_r_5_1[1234].csv'))
+VegList = sorted(glob.glob('../brainweb_new/result/generated_r_5_1[12].csv'))
 data = []
 
-#f = VegList[0]
-#with open(f, 'r') as file1:
-#    dat_iter = csv.reader(file1, skipinitialspace=True)
-#    data = [data for data in dat_iter]
-##    for row in dat_iter:
-##        print(row)
 
-#df = read_csv(f, header=None)
-#df[:10]
-#df2 = np.asarray(df)
-#df3 = df2.reshape(181,217,181,12)
 
 
 data = []
 for f in VegList:
     print(f)
-    # reader = csv.reader(f)
-    # dat = genfromtxt(f, delimiter=',')
-    #with open(f, 'r') as file:
-    #    dat_iter = csv.reader(file)
-    #all_dat = [all_dat, dat]
     dat_iter = np.asarray(read_csv(f, header=None))  ## np.asarray gives different format
     data.append(dat_iter.reshape(181, 217, 181, 12))
-    # VegListCoords = np.vstack((Veg.x, Veg.y, Veg.z)).transpose()
 
 # Maybe add pd.concat if you wish
-# data
-
 
 data_array = np.asarray(data)
 # print(data_array)
+data_array = data_array[:,:,:,:,0:5]
 print(data_array.shape)
 
 
@@ -124,22 +103,6 @@ TR_seq = (0.2, .2, 0.2, 0.2, 0.03, .03)
 
 
 
-### https://stackoverflow.com/questions/43915482/how-do-you-create-a-custom-activation-function-with-keras
-### See also, https://stackoverflow.com/questions/57023350/implementing-the-square-non-linearity-sqnl-activation-function-in-keras
-### and this: https://stackoverflow.com/questions/49982438/how-to-restrict-the-output-of-neural-network-to-be-positive-in-python-keras
-
-## Custom activation function
-#from keras.layers import Activation
-#from keras import backend as K
-#from keras.utils.generic_utils import get_custom_objects
-
-
-#def custom_activation(x):
-#    return (x * x)
-
-#get_custom_objects().update({'sq_activation': Activation(custom_activation)})
-
-
 
 ## cGAN generator: 
 
@@ -158,7 +121,6 @@ def build_generator(img_shape_8):
     print("Check 1")
     
     vis_final = Reshape((128,128,128,settings_no*3))(vis_final)								## Check the formation
-    # vis_final = Concatenate(axis=-1)([visible, TE_seq_1, TR_seq_1])
     
     print(vis_final.shape)
     print("\n\n\n\n\n")
@@ -167,22 +129,17 @@ def build_generator(img_shape_8):
 
     x1 = Dense(units=128, activation="sigmoid")(vis_final)
     x1 = Dense(units=1, activation="sigmoid")(x1)
-
-    x2 = Dense(units=128, activation="sigmoid")(vis_final)
-    x2 = Dense(units=1, activation="sigmoid")(x2)
-
-    joined_representation = Multiply()([x1, x2])
-    final = Dense(units=1, activation="elu")(joined_representation)
+    final = Dense(units=1, activation="elu")(x1)
 
     model = Model(inputs=[visible, TE_seq_1, TR_seq_1], outputs=final, name="try_gen")  ## No, this would have same dim as ??
 
-    plot_model(model, to_file='gan_gen.pdf', show_shapes=True, show_layer_names=True)
+    plot_model(model, to_file='gan_gen_test.pdf', show_shapes=True, show_layer_names=True)
     # print(model.summary())				# Added by Subrata
     return model
 
 
 
-build_generator(img_shape_8)
+# build_generator(img_shape_8)
 
 
 
@@ -198,103 +155,42 @@ def build_discriminator(img_shape):
     
     print("\nImage shape is:")
     print(visible.shape)
+    
+    x1_n = Conv3D(filters=1, kernel_size=64, strides=32, activation="elu",padding='same',name="x1_n")(visible)
+#    print("x1_n shape")
+#    print(x1_n.shape)
+    
+    x2_n = Conv3D(filters=1, kernel_size=64, strides=1, activation="elu",padding='same',name="x2_n")(visible)
+#    print("x2_n shape")
+#    print(x2_n.shape)
+    
+    x3_n = Conv3D(filters=1, kernel_size=64, strides=2, activation="elu",padding='same',name="x3_n")(visible)
+#    print("x3_n shape")
+#    print(x3_n.shape)
+    
+    x4_n = Conv3D(filters=1, kernel_size=64, strides=4, activation="elu",padding='same',name="x4_n")(visible)
+#    print("x4_n shape")
+#    print(x4_n.shape)
+    
+    x5_n = Conv3D(filters=1, kernel_size=64, strides=8, activation="elu",padding='same',name="x5_n")(visible)
+#    print("x5_n shape")
+#    print(x5_n.shape)
+    
+    x6_n = Conv3D(filters=1, kernel_size=64, strides=16, activation="elu",padding='same',name="x6_n")(visible)
+#    print("x6_n shape")
+#    print(x6_n.shape)
+    
+    
+    
+#    outputs_1 = []
+#    outputs_1.append(x1_n); outputs_1.append(x2_n); outputs_1.append(x3_n); outputs_1.append(x3_n); 
+#    outputs_1.append(x4_n); outputs_1.append(x5_n); outputs_1.append(x6_n);
+    # model = Model(inputs=visible, outputs=[x1_n, x2_n, x3_n, x4_n, x5_n, x6_n])
+#    model = Model(inputs=visible, outputs=outputs_1)
 
-    
-    u1 = Conv3D(filters=16, kernel_size=3, activation="elu",padding='same',name="conv_11")(visible)
-    u1 = BatchNormalization(name="BN1")(u1)
-    u1 = Conv3D(filters=16, kernel_size=3, activation="elu",padding='same',name="conv_12")(u1)
-    u1 = BatchNormalization(name="u1")(u1)
-    
-    v1 = Conv3D(filters=32, kernel_size=3, strides=2, activation="elu",padding='same',name="conv_1")(u1)
-    v1 = BatchNormalization(name="v1")(v1)
-    y21 = u1
-    
-        
-    u2 = Conv3D(filters=32, kernel_size=3, activation="elu",padding='same',name="conv_21")(v1)
-    u2 = BatchNormalization(name="BN2")(u2)
-    u2 = Conv3D(filters=32, kernel_size=3, activation="elu",padding='same',name="conv_22")(u2)
-    u2 = BatchNormalization(name="u2")(u2)
-    
-    v2 = Conv3D(filters=64, kernel_size=3, strides=2, activation="elu",padding='same',name="conv_2")(u2)
-    v2 = BatchNormalization(name="v2")(v2)
-    y22 = UpSampling3D(size = 2)(u2)
-    
-    
-    u3 = Conv3D(filters=64, kernel_size=3, activation="elu",padding='same',name="conv_31")(v2)
-    u3 = BatchNormalization(name="BN3")(u3)
-    u3 = Conv3D(filters=64, kernel_size=3, activation="elu",padding='same',name="conv_32")(u3)
-    u3 = BatchNormalization(name="u3")(u3)
-    
-    v3 = Conv3D(filters=128, kernel_size=3, strides=2, activation="elu",padding='same',name="conv_3")(u3)
-    v3 = BatchNormalization(name="v3")(v3)
-    y23 = UpSampling3D(size = 4)(u3)
-    
-    
-    u4 = Conv3D(filters=128, kernel_size=3, activation="elu",padding='same',name="conv_41")(v3)
-    u4 = BatchNormalization(name="BN4")(u4)
-    u4 = Conv3D(filters=128, kernel_size=3, activation="elu",padding='same',name="conv_42")(u4)
-    u4 = BatchNormalization(name="u4")(u4)
-    
-    v4 = Conv3D(filters=256, kernel_size=3, strides=2, activation="elu",padding='same',name="conv_4")(u4)
-    v4 = BatchNormalization(name="v4")(v4)
-    y24 = UpSampling3D(size = 8)(u4)
-    
-    
-    u5 = Conv3D(filters=256, kernel_size=3, activation="elu",padding='same',name="conv_51")(v4)
-    u5 = BatchNormalization(name="BN5")(u5)
-    u5 = Conv3D(filters=256, kernel_size=3, activation="elu",padding='same',name="conv_52")(u5)
-    u5 = BatchNormalization(name="u5")(u5)
-    
-    v5 = Conv3D(filters=512, kernel_size=3, strides=2, activation="elu",padding='same',name="conv_5")(u5)
-    v5 = BatchNormalization(name="v5")(v5)
-    y25 = UpSampling3D(size = 16)(u5)
-    y26 = UpSampling3D(size = 32)(v5)
-    
-    x1 = Conv3D(filters=1, kernel_size=3, strides=1, padding='same',name="x1")(v5)
-    
-    z2 = Concatenate(axis=-1)([y21, y22, y23, y24, y25, y26]) # Concatenates images with their label embeddings
-    x2 = Conv3D(filters=1, kernel_size=3, strides=1, padding='same',name="x2")(z2)
-    
-    
-    y32 = MaxPool3D(pool_size = 2,name="y32")(y22)
-    y33 = MaxPool3D(pool_size = 2,name="y33")(y23)
-    y34 = MaxPool3D(pool_size = 2,name="y34")(y24)
-    y35 = MaxPool3D(pool_size = 2,name="y35")(y25)
-    y36 = MaxPool3D(pool_size = 2,name="y36")(y26)
-        
-    z3 = Concatenate(axis=-1)([y32, y33, y34, y35, y36])
-    x3 = Conv3D(filters=1, kernel_size=3, strides=1, padding='same',name="x3")(z3)
-    
-    y43 = MaxPool3D(pool_size = 2,name="y43")(y33)
-    y44 = MaxPool3D(pool_size = 2,name="y44")(y34)
-    y45 = MaxPool3D(pool_size = 2,name="y45")(y35)
-    y46 = MaxPool3D(pool_size = 2,name="y46")(y36)
-
-    z4 = Concatenate(axis=-1)([y43, y44, y45, y46])
-    x4 = Conv3D(filters=1, kernel_size=3, strides=1, padding='same',name="x4")(z4)
-    
-    
-    y54 = MaxPool3D(pool_size = 2,name="y54")(y44)
-    y55 = MaxPool3D(pool_size = 2,name="y55")(y45)
-    y56 = MaxPool3D(pool_size = 2,name="y56")(y46)
-    z5 = Concatenate(axis=-1)([y54, y55, y56])
-    x5 = Conv3D(filters=1, kernel_size=3, strides=1, padding='same',name="x5")(z5)
-    
-    
-    y65 = MaxPool3D(pool_size = 2,name="y65")(y55)
-    y66 = MaxPool3D(pool_size = 2,name="y66")(y56)
-    
-    print("y65: "); print(y65.shape)
-    print("y66: "); print(y66.shape)
-    
-    z6 = Concatenate(axis=-1)([y65, y66])
-    print("z6: "); print(z6.shape)
-    x6 = Conv3D(filters=1, kernel_size=3, strides=1, padding='same',name="x6")(z6)
-    
-    
-    
-    # model = Model(inputs=visible, outputs=[x1, x2, x3, x4, x5, x6])
     ### Problem is in this line. 
+    
+    
     
     
     ## My fix: faltten and then connect using - https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=8662660
@@ -329,7 +225,11 @@ def build_discriminator(img_shape):
     x5_flat = Dense(units=1, activation="sigmoid")(x5_flat)
     x6_flat = Dense(units=1, activation="sigmoid")(x6_flat)
     
-        
+    print("x1: "); print(x1.shape)
+    print("x6: "); print(x6.shape)
+    print("x1_flat: "); print(x1_flat.shape)
+    print("x6_flat: "); print(x6_flat.shape)
+    
     final_1 = concatenate([x1_flat, x2_flat, x3_flat, x4_flat, x5_flat, x6_flat])
     final_2 = Dense(units=1, activation="sigmoid")(final_1)
     
@@ -340,19 +240,19 @@ def build_discriminator(img_shape):
     
     # compile model
     opt = Adam(lr=0.0002, beta_1=0.5)
-    model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'], options = run_opts)
     
-    plot_model(model, to_file='gan_dis.pdf', show_shapes=True, show_layer_names=True) # , expand_nested=True
+    plot_model(model, to_file='gan_dis_test.pdf', show_shapes=True, show_layer_names=True) # , expand_nested=True
     # model_to_dot(model, show_shapes=True, show_layer_names=True, expand_nested=True)
     #print(model.summary())
     return model
 
 
 
-build_discriminator(img_shape)
+#build_discriminator(img_shape)
 
 
-
+#print("\n\n\n\n\n\n\nDis and gen defined!!!!!")
 
 
 def define_gan(g_model, d_model):
@@ -362,6 +262,7 @@ def define_gan(g_model, d_model):
     gen_raw, gen_TE, gen_TR = g_model.input
     # get image output from the generator model
     gen_output = g_model.output
+    print("gen_output.shape:")
     print(gen_output.shape)  ## (None, 1, 1, 1) -- not matching with input of d_model in the next line
     # wants img_shape (128, 128, 1)
     
@@ -371,25 +272,26 @@ def define_gan(g_model, d_model):
     model = Model([gen_raw, gen_TE, gen_TR], gan_output)
     # compile model
     opt = Adam(lr=0.0002, beta_1=0.5)
-    model.compile(loss='binary_crossentropy', optimizer=opt)
-    plot_model(model, to_file='gan_all.pdf', show_shapes=True, show_layer_names=True)
+    model.compile(loss='binary_crossentropy', optimizer=opt, options = run_opts)
     return model
 
 
 
 
-# create the discriminator
-d_model = build_discriminator(img_shape)
-# create the generator
-g_model = build_generator(img_shape_8)
+## create the discriminator
+#d_model = build_discriminator(img_shape)
+## create the generator
+#g_model = build_generator(img_shape_8)
 
-d_model
+#d_model
 
-g_model
+#g_model
 
-# create the gan
-gan_model = define_gan(g_model, d_model)
-# load image data
+## create the gan
+#gan_model = define_gan(g_model, d_model)
+## load image data
+
+#print("\n\n\n\n\n\nGAN model defined!!!!!")
 
 
 
@@ -398,33 +300,51 @@ def generate_real_samples(dataset, n_batch):
     # choose random instances
     ix = randint(0, dataset.shape[1]-128); iy = randint(0, dataset.shape[2]-128); iz = randint(0, dataset.shape[3]-128)
     # select images and labels
-    X = dataset[:,ix:(ix+128), iy:(iy+128), iz:(iz+128),:]
-    return X
+    X = dataset[:,ix:(ix+128), iy:(iy+128), iz:(iz+128),:]	## (no_sample, X, Y, Z, settings_no)
+    return X, ix, iy, iz
+
+
+def generate_fake_samples(generator, dataset, ix, iy, iz, n_samples):
+	# generate points in latent space
+	# z_input, labels_input = generate_latent_points(latent_dim, n_samples)
+	# predict outputs
+	images = generator.predict([z_input, labels_input])
+	# create class labels
+	y = zeros((n_samples, 1))
+	return [images, labels_input], y
 
 
 
-X_real = generate_real_samples(data_array, 128)
+
+
+dataset = data_array
+
+X_real, ix, iy, iz = generate_real_samples(dataset, 128)
 print("X_real[0].shape")
 print(X_real[0].shape)
-print("X_real[1:3,:,:,:,0].shape: ")
-print(X_real[1:3,:,:,:,0].shape)
+print("X_real[:,:,:,:,0].shape: ")
+print(X_real[:,:,:,:,0].shape)
 
 # update discriminator model weights
-y_real = ones(((X_real[1:3,:,:,:,0]).shape[0], 1))
+y_real = ones((X_real.shape[0], 1))
 print("y_real shape: ")
 print(y_real.shape)
 
 d_model = build_discriminator(img_shape)
 
+# d_model.fit(x=X_real[:,:,:,:,0], y=y_real, batch_size=4, epochs=2)
+# Incompatible shapes: [2,1] vs. [2,16,16,16,1]
 
 print("Train: ")
-d_model.train_on_batch(X_real[1:3,:,:,:,0], y_real)
+
+# d_model.train_on_batch(X_real[:,:,:,:,0], y_real)
+
 
 
 print("Check 2")
 
 # generate 'fake' examples
-[X_fake, labels], y_fake = generate_fake_samples(g_model, latent_dim, half_batch)
+[X_fake, labels], y_fake = generate_fake_samples(g_model, dataset, ix, iy, iz, half_batch)
 print("Check 3")
 
 # update discriminator model weights
@@ -448,8 +368,6 @@ g_loss = gan_model.train_on_batch([z_input, labels_input], y_gan)
 
 
 
-
-
 def train1(g_model, d_model, gan_model, dataset, n_batch=128, n_iter=10):
     # manually enumerate epochs
     for i in range(n_iter):
@@ -458,13 +376,17 @@ def train1(g_model, d_model, gan_model, dataset, n_batch=128, n_iter=10):
             # get a 128x128x128 patch from the main data
             # No, only take the flair image from the data. 
             X_real = generate_real_samples(dataset, n_batch)
-            print("X_real.shape: ")
-            print(X_real.shape)
+            print("X_real[:,:,:,:,0].shape: ")
+            print(X_real[:,:,:,:,0].shape)
             
             # update discriminator model weights
             y_real = ones((X_real.shape[0], 1))
             print("y_real shape: ")
             print(y_real.shape)
+            
+            
+            ## Problem here: Changed the format of output. 
+            ## See this also: https://github.com/fengwang/MCNN/blob/master/tutorial/tutorial.ipynb            
             d_loss1, _ = d_model.train_on_batch(X_real[:,:,:,:,0], y_real)
             # discriminator model should get the pd image - check which one is the rho density
             print("Check 2")
@@ -504,6 +426,8 @@ def train1(g_model, d_model, gan_model, dataset, n_batch=128, n_iter=10):
 # dataset = load_real_samples()
 ## train model
 # train1(g_model, d_model, gan_model, data_array, 128, 10)
+
+
 
 
 
