e1729fb538e2d99c759351be71b40b2bf4bd8cc2
Subrata <subrata@iastate.edu>
2021-08-26 09:29:33 -0500

    Small changes

--- memcheck.R
+++ memcheck.R
@@ -1,4 +1,6 @@
-devtools::load_all()
-devtools::run_examples()
-devtools::build_vignettes()
+# https://stackoverflow.com/questions/65872960/use-valgrind-with-r-cmd-check
+
+#devtools::load_all()
+#devtools::run_examples()
+##devtools::build_vignettes()
 devtools::test()
